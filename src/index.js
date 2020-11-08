3d90b6b0099d937efc4fcf265f08d1243aeb3893
InayaVitoria <inayavitoria10@gmail.com>
2020-11-08 00:24:01 -0300

    hh

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ ReactDOM.render(
                   <h2>clique em um dos botões abaixo</h2>
                 </div>
                 <div id="select">
-                  <a href="${process.env.REACT_APP_API_URL}/pedidos"><button className="select_button">Produtos</button></a>
-                  <a href="${process.env.REACT_APP_API_URL}/carrinhos"><button className="select_button">Pedidos</button></a>
-                  <a href="${process.env.REACT_APP_API_URL}${process.env.REACT_APP_API_URL}/usuarios"><button className="select_button">Clientes</button></a>
+                  <a href="https://websiteback.herokuapp.com/pedidos"><button className="select_button">Produtos</button></a>
+                  <a href="https://websiteback.herokuapp.com/carrinhos"><button className="select_button">Pedidos</button></a>
+                  <a href="https://websiteback.herokuapp.com/usuarios"><button className="select_button">Clientes</button></a>
                 </div>
     </div>
     <App />
